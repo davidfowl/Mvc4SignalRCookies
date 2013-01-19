2ffc50ad679be3deb0614dc894e4e58cce6c2a01
David Fowler <davidfowl@gmail.com>
2013-01-19 15:27:54 -0800

    Mvc Cookies test.

---
+++ MvcApplication3/Hubs/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MvcApplication3.Hubs
+{
+    public class MyHub: Hub
+    {
+        public string GetCookie()
+        {
+            return Context.RequestCookies["x"].Value;
+        }
+    }
+}
